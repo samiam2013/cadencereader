9f5034c209acd966f18669c9f7f30603e621c199
samiam2013 <samiam2013@gmail.com>
2026-08-07 23:03:15 -0500

    open browser automatically

--- localdev/run.sh
+++ localdev/run.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+OS=$(uname)
+if [ $OS != 'Darwin' ]; then 
+    echo "not running on macos, is this prod?"; 
+    exit 1; 
+fi
+
+echo "compilation time:";
+TIMEFORMAT=%R;
+time go build .. ;
+if [ $? -ne 0 ]; then 
+    echo "build failed, check error above";
+    exit 1;
+fi
+
 set -a
 source .env
 set +a
-go run ..
+
+echo "starting broswer";
+(sleep 1; open http://localhost:8080) &
+./cadencereader
