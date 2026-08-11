44073a41e868ec5378b0539b0c48fa1cc3e6940c
samiam2013 <samiam2013@gmail.com>
2026-08-11 14:19:09 -0500

    add rssimport cron job

--- kapply.sh
+++ kapply.sh
@@ -4,6 +4,9 @@ set -e
 docker build -f apps/web/main/Dockerfile -t cadencereader:latest .
 docker save cadencereader:latest | sudo k3s ctr images import -
 
+docker build -f apps/rssimport/Dockerfile -t rssimport:latest .
+docker save rssimport:latest | sudo k3s ctr images import -
+
 kubectl apply -f k8s/
 kubectl rollout restart deployment cadencereader
-
+kubectl rollout restart cron rssimport
