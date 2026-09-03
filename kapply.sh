c1c5772243dd11603ce86331d9463f19bf1a8980
Sam Myres <sam@myres.dev>
2026-09-03 02:33:13 +0000

    add dripper, delete old immediate-rssimport jobs

--- kapply.sh
+++ kapply.sh
@@ -4,9 +4,13 @@ set -e
 docker build -f apps/web/main/Dockerfile -t cadencereader:latest .
 docker save cadencereader:latest | sudo k3s ctr images import -
 
+docker build -f apps/web/dripper/Dockerfile -t dripper:latest .
+docker save dripper:latest | sudo k3s ctr images import -
+
 docker build -f apps/rssimport/Dockerfile -t rssimport:latest .
 docker save rssimport:latest | sudo k3s ctr images import -
 
 kubectl apply -f k8s/
-kubectl rollout restart deployment cadencereader
+kubectl rollout restart deployment cadencereader 
+kubectl delete job immediate-rssimport # in case it already exists
 kubectl create job immediate-rssimport --from=cronjob/rssimport
